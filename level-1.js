710c85c55253c4a4d1610c1936ccd59c585cc3f5
matsbruun <55954689+matsbruun@users.noreply.github.com>
2020-01-18 20:54:53 +0100

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,28 @@
+// question 1
+var component = "cpu";
+
+// question 2
+var person = "male", "female";
+
+// question 3
+
+
+// question 4
+
+
+// question 5
+
+
+// question 6
+
+
+// question 7
+
+
+// question 8
+
+
+// question 9
+
+
+// question 10
